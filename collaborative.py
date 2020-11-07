d376ea4ec980c984537a4cd6678f72b30a335c68
Timothy Elgersma <timothy.elgersma@gmail.com>
2020-11-07 11:48:01 -0500

    open matrix

---
+++ collaborative.py
@@ -0,0 +1,71 @@
+import pandas
+from scipy.sparse import lil_matrix
+import sys
+import numpy as np
+from typing import List, Tuple, Dict
+import time
+import functools
+
+ORIGINAL_DATASET_SIZE = 44120615
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def timed(*args, **kw):
+        ts = time.time()
+        result = func(*args, **kw)
+        te = time.time()
+
+        print(f"{func.__name__:<20} {f'{(te-ts) * 100:,.2f} ms':>10}")
+        return result
+    return timed
+
+
+def get_collab_matrix(scale) -> Tuple[List[str], List[str], lil_matrix]:
+    """Return `n` user_ids, `m` track_ids, and an `n x m` sparse matrix"""
+
+    @timeit
+    def open_csv() -> pandas.DataFrame:
+        nrows = int(ORIGINAL_DATASET_SIZE * scale)
+        df = pandas.read_csv("triplets.csv", nrows=nrows)
+        print(f"Working with {nrows} rows")
+        return df
+
+    df = open_csv()
+
+    user_indices = {}
+    track_indices = {}
+
+    @timeit
+    def read_values() -> Tuple[List[str], List[str]]:
+        """Return a list of user IDs and a list of track IDs, that will
+        correspond to the rows/cols of the matrix"""
+        user_ids = df.iloc[:, 0].unique()
+        track_ids = df.iloc[:, 1].unique()
+
+        for i, user_id in enumerate(user_ids):
+            user_indices[user_id] = i
+        for i, track_id in enumerate(track_ids):
+            track_indices[track_id] = i
+        return user_ids, track_ids
+
+    user_ids, track_ids = read_values()
+
+    @timeit
+    def create_matrix() -> lil_matrix:
+        """Converts the list of triplets into a sparse matrix"""
+        M = lil_matrix((len(user_ids), len(track_ids)))
+        for i, row in df.iterrows():
+            user_ind = user_indices[row['user_id']]
+            track_ind = track_indices[row['song_id']]
+
+            M[user_ind, track_ind] = int(row['play_count'])
+        return M
+
+    return user_ids, track_ids, create_matrix()
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1] if len(sys.argv) > 1 else ""
+    scale = -arg.count("s")
+    user_labels, track_labels, M = get_collab_matrix(scale=10**scale)
